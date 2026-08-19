2c8d3a4d4fca5082b2c6fa85a61fbaa3a344fbb3
Dinu Ciobanu <dinucontdelucru@gmail.com>
2026-08-19 16:21:19 +0300

    make asana client singleton

--- api/client.go
+++ api/client.go
@@ -2,12 +2,21 @@ package api
 
 import (
 	"os"
+	"sync"
 
 	"resty.dev/v3"
 )
 
+var (
+	client     *resty.Client
+	clientOnce sync.Once
+)
+
 func AsanaClient() *resty.Client {
-	return resty.New().
-		SetBaseURL("https://app.asana.com/api/1.0").
-		SetAuthToken(os.Getenv("ACCESS_TOKEN"))
+	clientOnce.Do(func() {
+		client = resty.New().
+			SetBaseURL("https://app.asana.com/api/1.0").
+			SetAuthToken(os.Getenv("ACCESS_TOKEN"))
+	})
+	return client
 }
